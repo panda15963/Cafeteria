1d4a69a251c63e817259b65c04a8be928f4a27b4
MinseokChoi <seok7521@gmail.com>
2023-12-19 15:02:58 +0900

    master

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/NavBar";
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <Navbar />
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
