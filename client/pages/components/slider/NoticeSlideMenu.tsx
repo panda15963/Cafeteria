34e767ce0a4ef36e2c0f381ee0bfc05f5ae2be98
MinseokChoi <seok7521@gmail.com>
2024-01-31 15:34:37 +0900

    master

--- client/pages/components/slider/NoticeSlideMenu.tsx
+++ client/pages/components/slider/NoticeSlideMenu.tsx
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import data from "./NoticeMenuData";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Link from "next/link";
 function SampleNextArrow(props: any) {
   const { className, style, onClick } = props;
   return (
@@ -51,40 +50,39 @@ function SamplePrevArrow(props: any) {
     </div>
   );
 }
-export default class NoticeMenu extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      arrows: true,
-      fade: true,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />,
-    };
-    return (
-      <div className="container">
-        <h1 className="text-center text-4xl font-bold py-3">Notification Menu</h1>
-        <Slider {...settings}>
-          {data.map((item : any) => (
-            <div key={item.id}>
-              <div className="img-body">
-                <img
-                  className="img-fluid h-100 w-100"
-                  src={item.image}
-                  alt={item.title}
-                />
+const NoticeMenu = () => {
+  const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    fade: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <div className="container">
+      <h1 className="text-center text-4xl font-bold py-3">Notification Menu</h1>
+      <Slider {...settings}>
+        {data.map((item: any) => (
+          <Link key={item.id} href={`/components/menudetails/${item.title}`}>
+            <div className="img-body">
+              <img
+                className="img-fluid h-100 w-100"
+                src={item.image}
+                alt={item.title}
+              />
                 <h2>{item.title}</h2>
                 <p>{item.description}</p>
-              </div>
             </div>
-          ))}
-        </Slider>
-      </div>
-    );
-  }
-}
+          </Link>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+export default NoticeMenu;
